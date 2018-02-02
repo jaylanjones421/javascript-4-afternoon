4f029b95cff9d51266d5756b82dc6c5ad0bba948
Jaylan Jones <jaylanjones421@gmail.com>
2018-02-02 14:12:03 -0600

    up to problem 8 on closures

--- closures.js
+++ closures.js
@@ -22,14 +22,14 @@ function outer() {
   Invoke outer saving the return value into another variable called 'inner'.
 */
   
-// Code Here
+var inner  = outer();
 
 
 
 //Once you do that, invoke inner.
 
 //Code Here
-
+inner;
 
 
 ////////// PROBLEM 2 //////////
@@ -52,7 +52,8 @@ function callFriend(name) {
 */
 
 //Code Here
-
+var callJake = callFriend('Jake');
+callJake('435-555-9248');
 
 
 ////////// PROBLEM 3 //////////
@@ -62,15 +63,21 @@ function callFriend(name) {
 */
 
 //Code Here
-
+function makeCounter(){
+  var n=0;
+  return function(){
+    n++
+    return n;
+  }
+}
 
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -87,17 +94,24 @@ function callFriend(name) {
 
 function counterFactory(value) {
   // Code here.
-
+var num=value
   return {
-
+    inc: function(){
+      num++;
+      return num;
+    },
+    dec:function(){
+      num--
+      return num;
+    }
   };
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
+counter.inc() // 11
+counter.inc() // 12
+counter.inc() // 13
+counter.dec() // 12
 
 
 
@@ -113,9 +127,11 @@ function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
   // code message function here.
-
+function message(){
+return `${welcomeText} ${firstname} ${lastname}.`
+}
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -139,14 +155,19 @@ var module = (function() {
   function privateMethod(){
     return "Hi, I'm " + person.name + ", age " + person.age + " from " + person.location;
   }
-
+  
   // Anything that is being returned is made public and can be invoked from
   // outside our lexical scope
   return {
     // Code here.
+     publicMethod:function(){
+      return privateMethod();
+     }
+    
   };
 })();
 
+module.publicMethod()
 
 
 ////////// PROBLEM 7 //////////
@@ -163,6 +184,14 @@ function secretNumber() {
 
   return {
     // Code here
+    addToSecret:function(num){
+      secret+=num;
+      return secret
+    },
+    takeAwayFromSecret:function(num){
+      secret-=num;
+      return secret
+    }
   };
 }
 
